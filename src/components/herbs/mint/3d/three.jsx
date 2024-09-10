c1a7b1b10d00f13fba9aefb5e81491cd434c89ff
Ilan Usman <ilanusman03@gmail.com>
2024-09-10 10:40:00 +0530

    update 5.0

--- src/components/herbs/mint/3d/three.jsx
+++ src/components/herbs/mint/3d/three.jsx
@@ -8,8 +8,13 @@ const Three = () => {
   return (
 
     <div className="three">
+      <div className='box'>
         <Object />
-        <h1> MINT</h1>
+        </div>
+        <div className='details'>
+        <h1>Basil</h1>
+        <p>Basil (Ocimum basilicum) is a fragrant herb widely known for its medicinal and culinary uses. Often called the "king of herbs," basil is celebrated for its antibacterial, anti-inflammatory, and antioxidant properties. In traditional medicine, it is used to boost immunity, aid digestion, reduce stress, and improve respiratory health. With its rich aroma and diverse health benefits, basil is a staple in natural healing and daily wellness routines.</p>
+        </div>
     </div>
 
 
